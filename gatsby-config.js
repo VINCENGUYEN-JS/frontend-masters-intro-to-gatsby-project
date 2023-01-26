7af1c80b6de4d9bc4d0776f7c6a9071611d898f1
VinceNguyen <nguyenquangv95@gmail.com>
2023-01-26 21:15:42 +0700

    add-layout-and-seo

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://vincenguyen.com',
+    title: 'Intro to Gatsby',
+    description: 'Intro to Gatsby',
+    image:
+      'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
