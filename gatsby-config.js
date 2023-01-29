d7f3772922b5dabe34e894a3f54d20126a320b8c
VinceNguyen <nguyenquangv95@gmail.com>
2023-01-29 10:47:35 +0700

    add-mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,32 @@ module.exports = {
     image:
       'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    //load posts -> to pages ( default of gatsby is files in pages)
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    //turn them into pages
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    //turn mdx into content
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          posts: require.resolve('./src/components/post-layout.js'),
+        },
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,38 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout.js';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            title
+            description
+            date
+          }
+        }
+      }
+    }
+  `);
+  const posts = data.allMdx.nodes;
   return (
     <>
       <Layout>
         <h1>Hello World</h1>
         <Link to="/about">To About</Link>
+        <h2>Checkout my recent blog posts</h2>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link to={post.slug}> {post.frontmatter.title}</Link>{' '}
+              <small>posted {post.frontmatter.date}</small>
+            </li>
+          ))}
+        </ul>
       </Layout>
     </>
   );
